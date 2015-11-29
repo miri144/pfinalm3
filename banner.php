12edd7f7dd83506cf9bb6250e3ac7d8aebed625c
rtorrie <rmt225@cornell.edu>
2015-11-29 15:41:58 -0500

    Update banner.php

--- banner.php
+++ banner.php
@@ -5,10 +5,14 @@
         <div id="nav">
             <span><a href=index.php>Home</a></span>
             <span><a href=people.php>People</a></span>
-            <span><a href=presentations.php>Presentations</a></span>
-            <span><a href=contact.php>Contact Us</a></span>  
+            <span><a href=presentations.php>Presentations / Publications</a></span>
+            <span><a href=directions.php>Directions</a></span>
+            <span><a href=Events.php>Events</a></span>
+            <span><a href=contact.php>Contact Us</a></span> 
         </div>
     </div>
 <div class="identity_rmt">
-        <h2>Purpose and Identity Processes Lab</h2>
+    <h2>Purpose and Identity 
+        <br>
+        Processes Lab</h2>
         </div>
